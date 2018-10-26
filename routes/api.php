0b8e327c1b45247f539ea583607f84abd4ef8d2a
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-10-26 19:04:45 +0700

    create login endpoint

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,11 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['prefix' => 'auth'], function ($api) {
+        $api->POST('login', 'App\Http\Controllers\AuthController@login');
+    });
 });

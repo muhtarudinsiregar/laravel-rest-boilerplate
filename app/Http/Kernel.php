75aafa77351d9eab436ddc6a91ab0cf9c4c8be2b
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-10-28 21:37:53 +0700

    Setup jwt-auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,10 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // set middleware for jwt
+        'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
+        'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
     ];
 
     /**

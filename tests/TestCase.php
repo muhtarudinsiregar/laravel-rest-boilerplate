5d8e3be34a7dcf6f5071b6cefa18bfdc7b3923a0
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2018-12-10 13:12:58 +0700

    Fix: Cannot get token in testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,6 +25,8 @@ abstract class TestCase extends BaseTestCase
         return $headers;
     }
 
+    // source from https://github.com/astralapp/astral/blob/0ec1bb9f2df970fab6a2ee9130a6d421d0cdc73c/tests/TestCase.php
+
     public function login($user = null)
     {
         $user = $user ? : factory(User::class)->create();
@@ -33,5 +35,12 @@ abstract class TestCase extends BaseTestCase
         $this->headers['Authorization'] = 'Bearer ' . $token;
 
         return $this;
+    }
+
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $applicationJson = $this->transformHeadersToServerVars($this->headers);
+        $server = array_merge($server, $applicationJson);
+        return parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
     }
 }
